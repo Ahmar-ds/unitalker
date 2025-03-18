ca4c71d1ead97b3b0b53afd5fc86fae4bc87eef3
Ahmar-ds <bsdsf21m043@pucit.edu.pk>
2025-03-18 20:19:03 +0500

    Update api.py

--- api.py
+++ api.py
@@ -182,4 +182,4 @@ async def generate_animation(audio_file: UploadFile = File(..., description="WAV
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
